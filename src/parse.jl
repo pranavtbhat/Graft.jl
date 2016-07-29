c89b5821ebc842e202850226ab6f4ca6e0210caf
Pranav <pranavtbhat@gmail.com>
2016-07-29 12:53:22 +0530

    Much faster File IO

--- src/parse.jl
+++ src/parse.jl
@@ -4,17 +4,21 @@
 # However currently, only a type of CSV supported. The format is described below
 #
 # The input file should contain data in the following format:
-# <num_vertices>,<num_edges>
-#
-# <vertex_label>,<vertex_prop1>,<val_1>,<vertex_prop2>,<val_2> ...
+# <num_vertices>,<num_edges> (1)
+# <vprop1>,<vprop2>,<vprop3>... (2)
+# <vproptype1>,<vproptype2>,<vproptype3>... (3)
+# <eprop1>,<eprop2>,<eprop3>... (4)
+# <eproptype1>,<eproptype2>,<eproptype3>... (5)
+# <vertex_label>,<val_1>,<val_2>,<val_3> ... (6)
 # .
 # .
 # .
-#
-# <from_vertex_label>,<to_vertex_label>,<edge_property_1>,<val_1>,<edge_property_2>,<val_2> ...
+# <vertex_label>,<val_1>,<val_2>,<val_3> ... (Nv + 5)
+# <from_vertex_label>,<to_vertex_label>,<val_1>,<val_2> ... (Nv + 6)
 # .
 # .
 # .
+# <from_vertex_label>,<to_vertex_label>,<val_1>,<val_2> ... (Nv + Ne + 5)
 # EOF
 
 ################################################# IMPORT/EXPORT ############################################################
@@ -24,117 +28,134 @@ loadgraph,
 # Write Graphs
 storegraph
 
+################################################# HELPERS ##################################################################
 
+
+
+
+parseprops(x::SubString) = join(x)
+parseprops(x) = x
+parseprops(props::Vector) = map(parseprops, props)
+
+if CAN_USE_LG
+   function hackedges!(x::LightGraphsAM, es::Vector{EdgeID})
+      for e in es
+         addedge!(x, e...)
+      end
+   end
+end
+
+function hackedges!(x::SparseMatrixAM, es::Vector{EdgeID})
+   sort!(es)
+   x.ne = length(es)
+   x.fdata = init_spmx(nv(x), sort(es), fill(true, length(es)))
+   x.bdata = x.fdata'
+   nothing
+end
+
+
+# SubString Hack to Char, Bool
+Base.Char(x::SubString{String}) = x[1]
+Base.Bool(x::SubString{String}) = parse(x)
+
+promote_vector(x::DataType, y::Vector) = collect(x, y)
+promote_vector(::Type{Char}, y::Vector) = map(Char, y)
+promote_vector(::Type{Bool}, y::Vector) = map(Bool, x)
+
+function printif(cond, s::String)
+   if cond
+      println(s)
+   end
+end
 ################################################# READ GRAPHS ##############################################################
 
-# Parse token
-parseval(x::SubString) = parseval(join(x))
-# Prevent infinte loop
-function parseval(x::String)
-   s = parse(x)
-   isa(s, String) ? s : parseval(s)
-end
-parseval(x::Int) = x
-parseval(x::Float64) = x
-parseval(x::Char) = x
-parseval(x::Symbol) = isdefined(x) ? x : string(x)
-parseval(x::Bool) = x
-parseval(x::Void) = x
-parseval(x) = error("Didn't know what to do with -> $x")
+function loadgraph(io::IO, graph_type=SparseGraph, verbose=false)
+   arr = readcsv(io; skipblanks=false)
 
-# Parse opening declaration -> <num_vertices> <num_edges>
-parse_spec(args::Vector) = (parseval(args[1]), parseval(args[2]))
+   printif(verbose, "Fetching Graph Header")
+   Nv, Ne = collect(Int, arr[1,1:2])
 
-# Fetch, strip and split the next line from stream
-next_line(io::IO) = split(strip(readline(io), '\n'), ",")
+   vprops = parseprops(filter(x -> x != "", arr[2,:]))
+   vtypes = map(x->eval(parse(x)), filter(x -> x != "", arr[3,:]))
 
+   eprops = parseprops(filter(x -> x != "", arr[4,:]))
+   etypes = map(x->eval(parse(x)), filter(x -> x != "", arr[5,:]))
 
-# Parse a line describing a vertex -> <vertex_id> <vertex_prop_1> <val_1> <vertex_prop_2> <val_2> ...
-function parsevertex(g::Graph, v::VertexID, args::Vector)
-   vlabel = parseval(args[1])
-   for i in eachindex(args)[2:2:end-1]
-      propname = parseval(args[i])
-      val = parseval(args[i+1])
-      setvprop!(g, v, val, propname)
+   g = emptygraph(graph_type, Nv)
+
+   printif(verbose, "Fetching Vertex Labels")
+   labels = arr[6:(Nv+5), 1]
+   setlabel!(g, labels)
+
+   printif(verbose, "Fetching Vertex Data")
+   # vdata = arr[6:(Nv+6), 2:end]
+   for (i,prop) in enumerate(vprops)
+      vals = promote_vector(vtypes[i], arr[6:(Nv+5),i+1])
+      setvprop!(g, :, vals, prop)
    end
-   vlabel
-end
 
-# Parse a line describing an edge ->
-function parseedge(g::Graph, args::Vector)
-   v1 = resolve(g, parseval(args[1]))
-   v2 = resolve(g, parseval(args[2]))
-   addedge!(g, v1, v2)
+   printif(verbose, "Fetching Edges")
+   us = resolve(g, arr[(Nv+6):(Nv+5+Ne),1])
+   vs = resolve(g, arr[(Nv+6):(Nv+5+Ne),2])
+   es = map(EdgeID, us, vs)
 
-   for i in eachindex(args)[3:2:end-1]
-      propname = parseval(args[i])
-      val = parseval(args[i+1])
-      seteprop!(g, v1, v2, val, propname)
-   end
-end
+   printif(verbose, "Adding Edges")
+   # Can be optimized significantly, per Adjmodule type
+   hackedges!(adjmod(g), es)
 
-function loadgraph(io::IO, graph_type=SparseGraph)
-   nv, ne = parse_spec(next_line(io))
-   g = emptygraph(graph_type, nv)
-
-   # First blank line
-   next_line(io)
-
-   vlabels = [parsevertex(g, v, next_line(io)) for v in 1 : nv]
-   setlabel!(g, vlabels)
-
-   # Second blank line
-   next_line(io)
-
-   for i in 1 : ne
-      parseedge(g, next_line(io))
+   printif(verbose, "Fetching Edge Data")
+   # edata = arr[(Nv+6):(Nv+5+Ne), 3:end]
+   for (i,prop) in enumerate(eprops)
+      vals = promote_vector(etypes[i], arr[(Nv+6):(Nv+5+Ne),i+2])
+      seteprop!(g, :, vals, prop)
    end
 
    g
 end
 
 """ Parse a text file in the trivial graph format """
-function loadgraph(filename::String, graph_type=SparseGraph)
+function loadgraph(filename::String, graph_type=SparseGraph; verbose=false)
    file = open(filename)
-   g = loadgraph(file, graph_type)
+   g = loadgraph(file, graph_type, verbose)
    close(file)
    g
 end
 ################################################# WRITE GRAPHS ##############################################################
 
-# Serialize a value for storage
-prepval(x::Int) = string(x)
-prepval(x::Float64) = string(x)
-prepval(x::Char) = "\'$x\'"
-prepval(x::String) = "\"$x\""
-prepval(x::Bool) = string(x)
-prepval(x::Void) = ""
+getvproptype(x::VectorPM, prop) = eltype(vdata(x)[prop])
+getvproptype(x::LinearPM, prop) = vprops(x)[prop]
+
+geteproptype(x::VectorPM, prop) = eltype(edata(x)[prop])
+geteproptype(x::LinearPM, prop) = eprops(x)[prop]
 
 """ Write a graph to file """
 function storegraph(g::Graph, io::IO)
-   println(io, "$(nv(g)),$(ne(g))")
+   Nv,Ne = size(g)
 
-   println(io)
+   vprops = listvprops(g)
+   vtypes = map(x->getvproptype(propmod(g), x), vprops)
 
-   for v in vertices(g)
-      print(io, prepval(encode(g, v)))
-      for (prop,val) in getvprop(g, v)
-         print(io, ",",prepval(prop), ",", prepval(val))
-      end
-      println(io)
-   end
+   eprops = listeprops(g)
+   etypes = map(x->geteproptype(propmod(g), x), eprops)
 
-   println(io)
+   labels = encode(g, vertices(g))
+   vdata = hcat(labels, [getvprop(g, :, prop) for prop in vprops]...)
 
-   for e in edges(g)
-      v1 = prepval(encode(g, e.first))
-      v2 = prepval(encode(g, e.second))
-      print(io, v1, ",", v2)
-      for (prop,val) in geteprop(g, e)
-         print(io, ",", prepval(prop), ",", prepval(val))
-      end
-      println(io)
-   end
+   es = collect(edges(g))
+   uls = encode(g, map(x->x.first, es))
+   vls = encode(g, map(x->x.second, es))
+
+   edata = hcat(uls, vls, [geteprop(g, :, prop) for prop in eprops]...)
+
+   println(io, "$Nv,$Ne")
+   println(io, join(vprops, ','))
+   println(io, join(vtypes, ','))
+   println(io, join(eprops, ','))
+   println(io, join(etypes, ','))
+
+   writedlm(io, vdata, ',')
+   writedlm(io, edata, ',')
+   nothing
 end
 
 function storegraph(g::Graph, filename::String)
