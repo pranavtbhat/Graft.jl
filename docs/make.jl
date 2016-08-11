0b76e81761b8bbbf1318cbbfdd466df563319172
Pranav <pranavtbhat@gmail.com>
2016-08-11 12:33:38 +0530

    Change doc build to 0.5

--- docs/make.jl
+++ docs/make.jl
@@ -10,5 +10,6 @@ custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
 deploydocs(
    #options
    deps = custom_deps,
-   repo = "github.com/pranavtbhat/Graft.jl.git"
+   repo = "github.com/pranavtbhat/Graft.jl.git",
+   julia = "0.5"
 )
